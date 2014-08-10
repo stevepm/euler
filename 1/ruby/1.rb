06220b6c4aacaece015b2474b87121569a54ab95
stevepm <steven.magelowitz@gmail.com>
2014-08-10 13:09:06 -0600

    Completed first euler

--- 1/ruby/1.rb
+++ 1/ruby/1.rb
@@ -1,5 +1,23 @@
-require_relative '../../lib/euler.rb'
+class One
+  def initialize(number)
+    @number = number
+  end
 
-answer = 0
+  def multiples
+    multiples_array = []
+    @number.times do |i|
+      if ((i % 3 == 0) || (i % 5 == 0)) && i > 0
+        multiples_array << i
+      end
+    end
+    multiples_array
+  end
 
-puts answer
+  def sum_of_multiples
+    sum = 0
+    multiples.each do |multiple|
+      sum += multiple
+    end
+    sum
+  end
+end
